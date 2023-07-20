aaad112a4b68756469ec5e8eb9904f729599b819
kooyunhyung <kooyh108@naver.com>
2023-07-20 21:31:16 +0900

    upload code

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jpabook.jpashop.domain.item.DeliveryStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Delivery {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @JsonIgnore
+    @OneToOne(fetch = FetchType.LAZY, mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status;
+}
